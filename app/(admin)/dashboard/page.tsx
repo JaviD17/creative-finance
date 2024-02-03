a8807622b482e79b91aa663eca6dbb8610e4e3d3
Javier Dominguez II <javi7057@gmail.com>
2024-02-03 17:32:34 -0600

    added scatter chart

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -9,8 +9,10 @@ import {
   Title,
   Tooltip,
   Legend,
+  LineElement,
+  PointElement,
 } from "chart.js";
-import { Bar, Pie } from "react-chartjs-2";
+import { Bar, Pie, Scatter } from "react-chartjs-2";
 
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
@@ -19,73 +21,13 @@ ChartJS.register(
   ArcElement,
   CategoryScale,
   LinearScale,
+  PointElement,
   BarElement,
+  LineElement,
   Title,
   Tooltip,
   Legend
 );
-
-const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "top" as const,
-    },
-    title: {
-      display: true,
-      text: "Chart.js Bar Chart",
-    },
-  },
-};
-
-// const labels = ["January", "February", "March", "April", "May", "June", "July"];
-
-// const datasets = [
-//   {
-//     month: "January",
-//     data: 5,
-//   },
-//   {
-//     month: "February",
-//     data: 46,
-//   },
-//   {
-//     month: "March",
-//     data: 92,
-//   },
-//   {
-//     month: "April",
-//     data: 53,
-//   },
-//   {
-//     month: "May",
-//     data: 35,
-//   },
-//   {
-//     month: "June",
-//     data: 67,
-//   },
-//   {
-//     month: "July",
-//     data: 45,
-//   },
-// ];
-
-// export const data = {
-//   labels,
-//   datasets: [
-//     {
-//       label: "Dataset 1",
-//       data: datasets.map((data) => data.data),
-//       backgroundColor: "rgba(255, 99, 132, 0.5)",
-//     },
-//     {
-//       label: "Dataset 2",
-//       data: datasets.map((data) => data.data),
-//       backgroundColor: "rgba(53, 162, 235, 0.5)",
-//     },
-//   ],
-// };
 
 const DashboardPage = () => {
   const deals = useQuery(api.deals.getAll);
@@ -99,6 +41,31 @@ const DashboardPage = () => {
   const completedDeals = deals
     ?.map((deal) => deal.status)
     .filter((status) => status === "completed").length;
+
+  const options = {
+    // responsive: true,
+    scales: {
+      x: {
+        beginAtZero: true,
+      },
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  const data = {
+    datasets: [
+      {
+        label: "Amount Needed / Return Rate",
+        data: deals?.map((deal) => ({
+          x: deal.returnRate,
+          y: deal.amountNeeded,
+        })),
+        backgroundColor: "hsla(252, 95%, 85%, 1)",
+      },
+    ],
+  };
 
   const data2 = {
     labels: ["Open Deals", "Closed Deals", "Completed Deals"],
@@ -133,11 +100,17 @@ const DashboardPage = () => {
             {/* <div className="w-11/12 lg:max-w-md">
               <Bar data={data} options={options} />
             </div> */}
-            <div className="w-11/12 lg:max-w-sm mx-auto bg-black-100 py-12 rounded-sm border border-black-950 shadow-2xl shadow-black-950">
+            <div className="w-11/12 lg:max-w-sm mx-auto bg-black-100 py-8 px-2 rounded-sm border border-black-950 shadow-2xl shadow-black-950">
               <h3 className="text-center text-xl text-black-950 font-extrabold tracking-widest uppercase">
-                Deal Status YTD
+                Deal Status Distribution YTD
               </h3>
               <Pie data={data2} />
+            </div>
+            <div className="w-11/12 lg:max-w-sm mx-auto bg-black-100 py-8 px-2 rounded-sm border border-black-950 shadow-2xl shadow-black-950">
+              <h3 className="text-center text-xl text-black-950 font-extrabold tracking-widest uppercase">
+                Amount Needed vs Return Rate YTD
+              </h3>
+              <Scatter options={options} data={data} />
             </div>
           </div>
         </div>
